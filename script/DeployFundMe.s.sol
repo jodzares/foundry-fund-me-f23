0e3429ed738a224bd3462baccbd3e7a2cc0a68f5
jodzares <jodza.popovic@gmail.com>
2024-09-16 11:31:58 +0200

    chore: forge init

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {FundMe} from "../src/FundMe.sol";
+import {FundMe} from "src/FundMe.sol";
 
 contract DeployFundMe is Script {
     function run() external {
